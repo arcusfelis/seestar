25bcc1ef186a53cf38a5475a14435bf435a07044
Michael Uvarov <arcusfelis@gmail.com>
2016-02-19 17:13:58 +0100

    Wait for cassandra in travis

---
+++ wait_for_cassandra.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+erl -eval 'try lists:map(fun(N) -> case gen_tcp:connect("127.0.0.1", 9042, [], 1000) of {error,econnrefused} -> io:format("Retry, n=~p~n", [N]), timer:sleep(1000); _ -> exit(normal) end end, lists:seq(1,60)) after init:stop(0) end.'
